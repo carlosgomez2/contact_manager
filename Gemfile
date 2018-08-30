9be887d3666d35717a3224528e09f277da0905ee
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-08-30 10:44:40 -0500

    add glyphicons and better  styles for layout items

--- Gemfile
+++ Gemfile
@@ -28,3 +28,4 @@ end
 
 gem 'faker', '~> 1.9', '>= 1.9.1'
 gem 'bootstrap', '~> 4.1.3'
+gem 'glyphicons'
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module ContactManager
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # fonts  
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
