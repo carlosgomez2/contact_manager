91d6a220535026b0cdf53e2a184a336fd6311e2e
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-08-30 11:29:20 -0500

    included gravatars to contact images

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :group
+
+  def gravatar
+    size = "?s=200"
+    default = "?d=robohash"
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}#{size}#{default}"
+  end
 end
