86824f6d0c9fbbced185b8cca41e40436810ddeb
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-09-03 11:46:01 -0500

    reduced jquery-ui, implemented json response with jbuilder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,11 @@
 //= require jasny-bootstrap.min
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery-ui
+//= require jquery-ui/core
+//= require jquery-ui/widget
+//= require jquery-ui/position
+//= require jquery-ui/widgets/autocomplete
+//= require jquery-ui/widgets/menu
 //= require_tree .
 
 document.addEventListener("turbolinks:load", function () {
@@ -26,6 +30,7 @@ document.addEventListener("turbolinks:load", function () {
       minLength: 3,
       select: function(event, ui) {
         $('#term').val(ui.item.value);
+        $(this).closest('form').submit();
       }
     });
   });
