18566806ba197b0d4e277a2dfc33aa26f98af6f9
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-09-07 10:23:00 -0500

    implemented helper for image new form, fix search autocomplete message on form

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,14 @@
 module ContactsHelper
+
+  def contact_image(contact)
+    @contact = contact
+    if @contact.avatar.present?
+      @contact.avatar.url(:avatar)
+    elsif @contact.email.present? 
+      @contact.gravatar
+    else
+      "https://imgplaceholder.com/200x200?text=No+Image&font-family=OpenSans_Bold_Italic"
+    end
+  end
+
 end
