e16bfe212a6d4738b574122d5b840641b7cad0fe
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-09-07 09:49:27 -0500

    fix invalid class input, fix hover btn save color

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -10,8 +10,8 @@ document.addEventListener("turbolinks:load", function () {
   $('#save-group-btn').click(function (event) {
     event.preventDefault();
 
-    var newGroup = $('#new_group');
-    var inputGroup = newGroup.closest('.input-group');
+    let newGroup = $('#new_group');
+    let inputGroup = newGroup.closest('.input-group');
 
     $.ajax({
       url: "/groups",
@@ -20,9 +20,10 @@ document.addEventListener("turbolinks:load", function () {
       data: {
         group: { name: $('#new_group').val() }
       },
+
       success: function (group) {
         if (group.id != null) {
-          inputGroup.removeClass('has-error');
+          newGroup.removeClass('is-invalid');
           inputGroup.next('.text-danger').remove();
 
           var newOption = $('<option />')
@@ -35,16 +36,14 @@ document.addEventListener("turbolinks:load", function () {
           newGroup.val("");
         }
       },
+
       error: function (xhr) {
         var errors = xhr.responseJSON;
-
-        console.log(errors);
-        console.log("errors");
         var error = errors.join(", ");
         if (error) {
           inputGroup.next('.text-danger').detach();
+          newGroup.addClass('is-invalid');
           inputGroup
-            .addClass('is-invalid')
             .after('<p class="text-danger">' + error + '</p>');
         }
       }
