f7e79a2f8f5f1d417fde0105954a24737da88491
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-09-09 17:25:57 -0500

    simple fix for flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def conversion_alert(type)
+    case type
+    when "notice"
+      type = "primary"
+    when "error"
+      type = "danger"
+    when "success"
+      type = "success"
+    when "alert"
+      type = "info"
+    else
+      type = "dark"
+    end
+  end
 end
