cadb0506b30f1338f05c305ddf8f10b792f12327
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-09-10 14:59:07 -0500

    included home landing design and dashboard design, change contacts structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -6,5 +7,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 end
