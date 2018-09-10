a3646862616671cefff4571d6cbe86bd73112270
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-09-10 09:18:29 -0500

    set current user name btn, created  home and dashboard controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
+  get 'home/index'
+
   devise_for :users
-  root 'contacts#index'
+  
+  root 'home#index'
+  
   resources :contacts, except: :show do
     get 'autocomplete', on: :collection
   end
 
   post 'groups', to: 'groups#create'
+  get '/dashboard', to: 'dashboard#index'
 
 end
